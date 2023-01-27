5125c45b400d81f2e87e34abea5ce16f8c2d5c75
ishwor <asishwor@gmail.com>
2023-01-27 14:42:57 +0545

    added widget on footer and styling them

--- footer.php
+++ footer.php
@@ -1,8 +1,42 @@
 <footer>
-    <div class="footer">
-        <h2>This is footer of this template</h2>
+    <div class="container">
+        <div class="footer">
+
+            <div>
+
+                <?php
+                dynamic_sidebar('footer-1');
+
+                ?>
+            </div>
+            <div>
+
+                <?php
+                dynamic_sidebar('footer-2');
+
+                ?>
+            </div>
+            <div>
+
+                <?php
+                dynamic_sidebar('footer-3');
+
+                ?>
+            </div>
+            <div>
+
+                <?php
+                dynamic_sidebar('footer-4');
+
+                ?>
+            </div>
+        </div>
+
     </div>
 </footer>
+<?php wp_enqueue_script('script_name', get_template_directory_uri() . "/js/main.js", 'jquery', false, true); ?>
+<?php wp_footer(); ?>
+
 </body>
 
 </html>
