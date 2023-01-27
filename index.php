a62e0ff7457c74393702d62410f4afbf51cc4c1f
ishwor <asishwor@gmail.com>
2023-01-27 14:48:06 +0545

    added sidebar

--- index.php
+++ index.php
@@ -9,81 +9,42 @@ the_post();
 </div>
 <div class="container">
     <main class="site__main">
-        <aside class="blog__sidebar">
-            <section>
-                <div>
-                    <h2 class="recent__blog-title">Recent posts</h2>
-                    <!-- all post are print here -->
-                    <?php get_sidebar(); ?>
+
+        <?php get_sidebar() ?>
+        <!-- all post are print here -->
+        <div class="blog-container">
+            <?php
+            while (have_posts()) {
+                the_post();
+
+                ?>
+            <div class="blog">
+                <a href="blog-image">
+                    <?php the_post_thumbnail('large') ?>
+                </a>
+                <div class="blog__content">
+                    <h4 class="blog__title">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php echo the_title(); ?></a>
+                    </h4>
+                    <p class="blog__excerpt">
+                        <?php the_excerpt(); ?>
+                    </p>
+                    <span class="blog__data">
+                        <?php the_date(); ?>
+                    </span>
+                    <a href="<?php the_permalink(); ?>">
+                        <button class="btn blog__btn"><span>Read more</span> <i
+                                class="fa-solid fa-arrow-right"></i></button>
+                    </a>
                 </div>
-                <h2>Latest News</h2>
+            </div>
+            <?php }
+            ; ?>
 
-                <?php $postType = array(
-          'post_type' => 'sports',
-          'post_status' => 'publish'
-        );
-        $newsQuery = new Wp_Query($postType);
-        while ($newsQuery->have_posts()) {
-          $newsQuery->the_post();
-          $imagePath = wp_get_attachment_image_src(get_post_thumbnail_id(), 'thumbnail');
-          echo '<div class="recent__blog">
-       
-            <a href=""> <img src="' . $imagePath[0] . '" alt="" /></a>
-            <div class="blog__sidebar-content">
-              <h4 class="blog__sidebar-title">
-              <a href="' . the_permalink() . '">
-            ' . the_title() . '</a>
-              </h4>
-              <span class="blog__sidebar-date">
-                ' . the_date() . '
-              </span>
-              <p class="blog__sidebar-excerpt">
-                <?php the_excerpt(); ?>
-                </p>
-</div>
-</div>';
-}
-
-
-
-?>
-</section>
-</aside>
-
-<!-- all post are print here -->
-<div class="blog-container">
-    <?php
-      while (have_posts()) {
-        the_post();
-
-        ?>
-    <div class="blog">
-        <?php
-          $imagePath = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large')
-            ?>
-        <img src="<?php echo $imagePath[0]; ?>" alt="" />
-        <div class="blog__content">
-            <h4 class="blog__title">
-                <a href="<?php the_permalink(); ?>">
-                    <?php echo the_title(); ?></a>
-            </h4>
-            <p class="blog__excerpt">
-                <?php the_excerpt(); ?>
-            </p>
-            <span class="blog__data">
-                <?php the_date(); ?>
-            </span>
-            <a href="<?php the_permalink(); ?>">
-                <button class="btn blog__btn"><span>Read more</span> <i class="fa-solid fa-arrow-right"></i></button>
-            </a>
         </div>
-    </div>
-    <?php }
-      ; ?>
 
-</div>
-
-</main>
+    </main>
 </div>
 
 <?php get_footer() ?>
