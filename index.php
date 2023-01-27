88b7064cd9ea1279d6fd7aae5a1b3d72a2a0ae72
ishwor <asishwor@gmail.com>
2023-01-27 14:42:26 +0545

    added side bar

--- index.php
+++ index.php
@@ -1,78 +1,89 @@
 <?php
 get_header();
+the_post();
 ?>
 <div class="blog__banner">
-  <div class="container">
-    <h2>Blog</h2>
-  </div>
+    <div class="container">
+        <h2>Blog</h2>
+    </div>
 </div>
 <div class="container">
-  <main class="site__main">
-    <aside class="blog__sidebar">
-      <section>
-        <div>
-          <h2 class="recent__blog-title">Recent posts</h2>
-          <!-- all post are print here -->
-          <div class="blog__sidebar-container">
-            <?php
-            while (have_posts()) {
-              the_post();
-
-              ?>
-              <div class="recent__blog">
-                <?php
-                $imagePath = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large')
-                  ?>
-                <a href=""> <img src="<?php echo $imagePath[0]; ?>" alt="" /></a>
-                <div class="blog__sidebar-content">
-                  <h4 class="blog__sidebar-title">
-                    <?php echo the_title(); ?>
-                  </h4>
-                  <span class="blog__sidebar-date">
-                    <?php the_date(); ?>
-                  </span>
-                  <p class="blog__sidebar-excerpt">
-                    <?php the_excerpt(); ?>
-                  </p>
+    <main class="site__main">
+        <aside class="blog__sidebar">
+            <section>
+                <div>
+                    <h2 class="recent__blog-title">Recent posts</h2>
+                    <!-- all post are print here -->
+                    <?php get_sidebar(); ?>
                 </div>
-              </div>
-            <?php }
-            ; ?>
-          </div>
-        </div>
-      </section>
-    </aside>
-    <!-- all post are print here -->
-    <div class="blog-container">
-      <?php
+                <h2>Latest News</h2>
+
+                <?php $postType = array(
+          'post_type' => 'sports',
+          'post_status' => 'publish'
+        );
+        $newsQuery = new Wp_Query($postType);
+        while ($newsQuery->have_posts()) {
+          $newsQuery->the_post();
+          $imagePath = wp_get_attachment_image_src(get_post_thumbnail_id(), 'thumbnail');
+          echo '<div class="recent__blog">
+       
+            <a href=""> <img src="' . $imagePath[0] . '" alt="" /></a>
+            <div class="blog__sidebar-content">
+              <h4 class="blog__sidebar-title">
+              <a href="' . the_permalink() . '">
+            ' . the_title() . '</a>
+              </h4>
+              <span class="blog__sidebar-date">
+                ' . the_date() . '
+              </span>
+              <p class="blog__sidebar-excerpt">
+                <?php the_excerpt(); ?>
+                </p>
+</div>
+</div>';
+}
+
+
+
+?>
+</section>
+</aside>
+
+<!-- all post are print here -->
+<div class="blog-container">
+    <?php
       while (have_posts()) {
         the_post();
 
         ?>
-        <div class="blog">
-          <?php
+    <div class="blog">
+        <?php
           $imagePath = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large')
             ?>
-          <img src="<?php echo $imagePath[0]; ?>" alt="" />
-          <div class="blog__content">
+        <img src="<?php echo $imagePath[0]; ?>" alt="" />
+        <div class="blog__content">
             <h4 class="blog__title">
-              <?php echo the_title(); ?>
+                <a href="<?php the_permalink(); ?>">
+                    <?php echo the_title(); ?></a>
             </h4>
             <p class="blog__excerpt">
-              <?php the_excerpt(); ?>
+                <?php the_excerpt(); ?>
             </p>
             <span class="blog__data">
-              <?php the_date(); ?>
+                <?php the_date(); ?>
             </span>
-            <button class="btn blog__btn"><span>Read more</span> <i class="fa-solid fa-arrow-right"></i></button>
-          </div>
+            <a href="<?php the_permalink(); ?>">
+                <button class="btn blog__btn"><span>Read more</span> <i class="fa-solid fa-arrow-right"></i></button>
+            </a>
         </div>
-      <?php }
-      ; ?>
-      <?php wp_pagenavi(); ?>
-
     </div>
-  </main>
+    <?php }
+      ; ?>
+
+</div>
+
+</main>
 </div>
 
 <?php get_footer() ?>
